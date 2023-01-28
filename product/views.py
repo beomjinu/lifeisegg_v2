ea7257c85f28125ead91b026f47dd5c830db14c3
beomjinu <ulhangry.dev@gmail.com>
2023-01-28 21:32:09 +0900

    Update Product App

--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    context = {
+        "title"     : "제품",
+    }
+    return render(request, "product/product.html", context)
