0f4f24bc7a5b8deb51d465f0172cf302dbad75f1
beomjinu <ulhangry.dev@gmail.com>
2023-01-28 15:05:25 +0900

    media 추ㄱ

--- lifeisegg/settings.py
+++ lifeisegg/settings.py
@@ -118,3 +118,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# media 추가
+MEDIA_ROOT  = os.path.join(BASE_DIR, "media")
+MEDIA_URL   = "/media/"
